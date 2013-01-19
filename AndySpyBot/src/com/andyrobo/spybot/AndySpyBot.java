83a16a4829ef7d302a89639ce88abd4e6fb9dc3c
abheek <abheek@robots-alive.com>
2013-01-19 17:56:38 +0530

    Adding UI elements and linking to AndyBase

--- AndySpyBot/src/com/andyrobo/spybot/AndySpyBot.java
+++ AndySpyBot/src/com/andyrobo/spybot/AndySpyBot.java
@@ -1,15 +1,55 @@
 package com.andyrobo.spybot;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.SurfaceView;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.ToggleButton;
 
-public class AndySpyBot extends Activity {
+import com.andyrobo.base.AndyActivity;
+import com.andyrobo.base.functions.AndySpyCam;
+import com.andyrobo.base.utils.AndyUtils;
+
+public class AndySpyBot extends AndyActivity {
+
+	private EditText ipAddress;
+	private final AndySpyCam cam = AndySpyCam.SPYCAM;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_andy_spy_bot);
+
+		ipAddress = (EditText) findViewById(R.id.etSendIP);
+		SurfaceView sView = (SurfaceView) findViewById(R.id.surfaceView);
+
+		ipAddress.setText(AndyUtils.getWifiAddress(this));
+		cam.init(sView);
+
+		ToggleButton spyToggle = (ToggleButton) findViewById(R.id.tbtnSpyMode);
+		spyToggle.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton arg0, boolean isChecked) {
+				handleSpyToggle(isChecked);
+			}
+		});
+	}
+
+	protected void handleSpyToggle(boolean isChecked) {
+		if (isChecked) {
+			if (cam.setReceiverIP(getIP())) {
+				cam.startSpyMode();
+			}
+		} else {
+			cam.stopSpyMode();
+		}
+	}
+
+	private String getIP() {
+		return ipAddress.getText().toString();
 	}
 
 	@Override
