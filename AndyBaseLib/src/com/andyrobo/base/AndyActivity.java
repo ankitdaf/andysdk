47fc9550a2e734af1c426fd3fe0dcc573d14730c
Ankit Daftery <ankitdaf@gmail.com>
2013-03-15 15:17:37 +0530

    Removed override notation

--- AndyBaseLib/src/com/andyrobo/base/AndyActivity.java
+++ AndyBaseLib/src/com/andyrobo/base/AndyActivity.java
@@ -26,8 +26,8 @@ public abstract class AndyActivity extends Activity {
 		WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	}
 	
-	@Override
-	protected final void onCreate(Bundle savedInstanceState) {
+	
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
